5419646d0e6d8c87c0a609626a8c650bf695fb62
aran <rnmhdn@gmail.com>
2019-07-02 16:09:14 +0430

    lexer finished

--- lexer.py
+++ lexer.py
@@ -1,8 +1,4 @@
 import string
-from collections import defaultdict
-
-with open("program.d", "r") as inn:
-    program = inn.read()
 
 keywords = [
     "if",
@@ -21,14 +17,10 @@ letters = string.ascii_letters
 digits = string.digits
 symbols = "()[]{};:<,+-*="
 whitespace = string.whitespace  # ' \t\n\r\x0b\x0c'
+terminal = whitespace + symbols
+alphanum = digits + letters
+charset = terminal + alphanum
 
-state = "START"
-COMMENT_FSM = {
-    "/": {"/": "//", "*": "/*..."},
-    "//": defaultdict(lambda: "//", {"\n": "START"}),
-    "/*...": defaultdict(lambda: "/*...", {"*": "/*...*", "\n": "/*...\n"}),
-    "/*...*": defaultdict(lambda: "/*...", {"/": "START", "\n": "/*...\n"}),
-}
 
 FSM = {
     "START": {
@@ -38,78 +30,111 @@ FSM = {
         **dict.fromkeys(digits, "NUM"),
         "=": "=",
         "/": "COMMENT",
-        "\n": "STOP",
     },
     "ID": {
-        **dict.fromkeys(whitespace + symbols, "STOP"),
-        **dict.fromkeys(digits + letters, "ID"),
+        **dict.fromkeys(terminal, "STOP"),
+        **dict.fromkeys(alphanum, "ID"),
+        "/": "STOP",
     },
     "NUM": {
-        **dict.fromkeys(whitespace + symbols, "STOP"),
+        **dict.fromkeys(terminal, "STOP"),
         **dict.fromkeys(letters, "ERROR"),
         **dict.fromkeys(digits, "NUM"),
+        "/": "STOP",
     },
-    "SYMBOL": {**dict.fromkeys(letters + digits + whitespace + symbols, "STOP")},
-    "=": {**dict.fromkeys(letters + digits + whitespace + symbols, "STOP"), "=": "=="},
-    "==": {**dict.fromkeys(letters + digits + whitespace + symbols, "STOP")},
-    "ERROR": None,
+    "SYMBOL": {**dict.fromkeys(charset, "STOP"), "/": "STOP"},
+    "=": {**dict.fromkeys(charset, "STOP"), "=": "==", "/": "STOP"},
+    "==": {**dict.fromkeys(charset, "STOP"), "/": "STOP"},
+    "ERROR": {
+        **dict.fromkeys(alphanum, "ERROR"),
+        **dict.fromkeys(terminal, "STOP"),
+        "/": "STOP",
+    },
     "STOP": None,
-    "COMMENT": None,  # We either have an error or
-}  # manage comments out of FSM
+    "COMMENT": None,  # We either have an error or there is a comment to skip
+}
+
+line_number = 1
+
+
+def nextt(iterator):
+    global line_number
+    char = next(iterator)
+    if char == "\n":
+        line_number += 1
+    return char
 
 
 def panic(msg):
-    return "({}, invalid input)".format(msg)
+    global line_number
+    return f"Line: {line_number}: ({msg}, invalid input)"
 
 
-def output(token, token_type):
-    return "({}, {})".format(token_type, token)
+def output(token_type, token):
+    if token_type in ["=", "=="]:
+        token_type = "SYMBOL"
+    if token_type == "ID" and token in keywords:
+        token_type = "KEYWORD"
+    return f"({token_type}, {token})"
 
 
-def get_next_token(prog, out="", err="", look_ahead=""):
-    token = ""
+def get_nextt_token(prog, look_ahead=""):
     if not look_ahead:
-        token += next(prog)
+        token = nextt(prog)
     else:
-        token += look_ahead
-    # first character of token in place
-    # if token[-1] == "/":
-    #     look_ahead = next(prog)
-    #     if look_ahead == "*":
-    #         try:
-    #             # this is some genius that just skips all chars until it finds '*/'
-    #             while next(prog) != "*" or next(prog) != "/":
-    #                 pass
-    #         except StopIteration:
-    #             return "", panic("/*")
-    #         return get_next_token(prog)
-    #     elif look_ahead == "/":
-    #         return out, err
-    #     else:
-    #         err += panic(token)
-    #         return get_next_token(prog, err=err, look_ahead=look_ahead)
-    # # comments handled
-    # else:
+        token = look_ahead
     initial_state = state = FSM["START"].get(token[-1], "ERROR")
-    if state == "ERROR":
-        return panic(token)
+    while state == "START":
+        token = nextt(prog)
+        initial_state = state = FSM[state].get(token[-1], "ERROR")
+    if state == "COMMENT":
+        look_ahead = nextt(prog)
+        if look_ahead == "*":
+            try:
+                # skip until '*/'
+                while nextt(prog) != "*" or nextt(prog) != "/":
+                    pass
+            except StopIteration:
+                return panic("/*"), ""
+            return get_nextt_token(prog)
+        elif look_ahead == "/":
+            try:
+                while nextt(prog) != "\n":
+                    pass
+            except StopIteration:
+                return "", ""  # think about this
+            return get_nextt_token(prog)
+        elif look_ahead in whitespace:
+            return panic("/"), ""
+        else:
+            state = "ERROR"
+            token += look_ahead
+            look_ahead = ""
     while state != "STOP":
-        token += next(prog)
+        token += nextt(prog)
         # remember to remove the last element of token if there is no error, this is probably the best way to do it, think before changing.
+        initial_state = state
         state = FSM[state].get(token[-1], "ERROR")
-    if state == "ERROR":
-        return panic(token)
+    if initial_state == "ERROR":
+        return panic(token[:-1]), token[-1]
     else:
-        look_ahead = token[-1]
-        token = token[:-1]
-        # removed the last element of token, token is clean now.
-        out += str((initial_state, token))
+        return (
+            output(initial_state, token[:-1]),
+            token[-1],
+        )  # removed the last element of token
 
 
+with open("program.sex", "r") as inn:
+    prog = iter(inn.read())
 with open("scanner.txt", "w") as out, open("lexical_errors.txt", "w") as err:
-    program_lines = "shit"
-    for idx, line in enumerate(program_lines):
-        outt = error = look_ahead = ""
-        while True:
-            outt, error, look_ahead = get_next_token(iter(line))
-    print("done")
+    outt = look_ahead = ""
+    while True:
+        try:
+            outt, look_ahead = get_nextt_token(prog, look_ahead)
+            if outt.endswith(", invalid input)"):
+                err.write(outt + "\n")
+            else:
+                out.write(outt + "\n")
+        except StopIteration:
+            print("done")
+            break
